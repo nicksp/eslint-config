f45195f60f2a74d0f7d321b99509db0f641dd87e
Nick Plekhanov <nicksp@users.noreply.github.com>
2025-11-06 03:50:57 +0100

    chore: update Vitest rules

--- src/configs/test.ts
+++ src/configs/test.ts
@@ -36,14 +36,29 @@ export function test(
       files,
       name: 'nicksp/test/rules',
       rules: {
+        // Consistently use *.test.ts instead of .spec.ts
+        'test/consistent-test-filename': [
+          'error',
+          { pattern: String.raw`.*\.test\.[tj]sx?$` },
+        ],
+        // Consistently use it() instead of test()
         'test/consistent-test-it': [
           'error',
           { fn: 'it', withinDescribe: 'it' },
         ],
+        // Consistently use vi.* instead of vitest.*
+        'test/consistent-vitest-vi': 'error',
+        // Disallow focused tests such as test.only()
+        'test/no-focused-tests': isInEditor ? 'warn' : 'error',
         'test/no-identical-title': 'error',
         'test/no-import-node-test': 'error',
-        'test/no-only-tests': isInEditor ? 'warn' : 'error',
-        'test/padding-around-all': 'error',
+        // Enforce padding around vitest functions except for padding-around-expect-groups
+        'test/padding-around-after-all-blocks': isInEditor ? 'warn' : 'error',
+        'test/padding-around-after-each-blocks': isInEditor ? 'warn' : 'error',
+        'test/padding-around-before-all-blocks': isInEditor ? 'warn' : 'error',
+        'test/padding-around-before-each-blocks': isInEditor ? 'warn' : 'error',
+        'test/padding-around-describe-blocks': isInEditor ? 'warn' : 'error',
+        'test/padding-around-test-blocks': isInEditor ? 'warn' : 'error',
         'test/prefer-hooks-in-order': 'error',
         'test/prefer-lowercase-title': [
           'error',
